1c88672f4d0a3dfec03b7a902447ec29a31515a4
igbanibo-Dengi <igbanibodengi@gmail.com>
2024-01-30 19:57:36 +0100

    ui changes

--- app/(root)/loadouts/[id]/page.tsx
+++ app/(root)/loadouts/[id]/page.tsx
@@ -14,20 +14,21 @@ const LoadoutDetails = async ({ params: { id }, searchParams }: SearchParamProps
         <>
             <div className='container pt-20 max-w-6xl'>
                 <div className='flex flex-col lg:flex-row gap-5 md:gap-10 justify-between mx-auto mt-20'>
-                    <div className='w-full'>
+                    <div className='md:w-[65%] w-full border rounded-md bg-[#101012]'>
                         <Image
                             src={loadout.imageUrl}
                             alt="hero image"
                             width={1000}
                             height={500}
                             priority
-                            className="h-full  rounded-md"
+                            className="h-full w-full rounded-md"
                         />
                     </div>
-                    <div className='w-full md:w-1/2 flex gap-4'>
-                        <div className='flex flex-col h-full w-full justify-betweenw-full py-5 whitespace-nowrap'>
-                            <div className='space-y-4 my-auto  w-full pb-4'>
-                                <p className='flex justify-between items-center'><span className='text-primary'>Title</span> <span>{loadout.title}</span></p>
+                    <div className='md:w-[35%] w-full flex gap-4  border rounded-md bg-[#101012] p-5'>
+                        <div className='flex flex-col h-full justify-between w-full py-5 whitespace-nowrap'>
+                            <p className='text-xl md:text-3xl font-bold capitalize mb-3'>{loadout.title}</p>
+                            <Separator />
+                            <div className='space-y-4 w-full pb-4 mt-2'>
                                 <p className='flex justify-between items-center'><span className='text-primary'>Weapon</span> <span>{loadout.weapon}</span></p>
                                 <p className='flex justify-between items-center'><span className='text-primary'>Category</span> <span> {loadout.category.name}</span></p>
                                 <p className='flex justify-between items-center'><span className='text-primary'>Mode</span>  <span>{loadout.gameMode}</span></p>
@@ -39,40 +40,48 @@ const LoadoutDetails = async ({ params: { id }, searchParams }: SearchParamProps
 
                     </div>
                 </div>
-                <div className='flex flex-col  md:flex-row-reverse gap-10 my-10'>
-                    <div className='flex flex-col gap-4 w-full md:pl-5'>
-                        <div className='w-full flex justify-between items-center text-primary'>
-                            <span className='flex gap-3 items-center'>
-                                <GoDotFill /> <p className='text-foreground'>{loadout.firstSlot}</p>
-                            </span>
-                            <p className='text-foreground'>{loadout.firstAttachment}</p>
-                        </div>
-                        <div className='w-full flex justify-between items-center text-primary'>
-                            <span className='flex gap-3 items-center'>
-                                <GoDotFill /> <p className='text-foreground'>{loadout.secondSlot}</p>
-                            </span>
-                            <p className='text-foreground'>{loadout.secondAttachment}</p>
-                        </div>
-                        <div className='w-full flex justify-between items-center text-primary'>
-                            <span className='flex gap-3 items-center'>
-                                <GoDotFill /> <p className='text-foreground'>{loadout.thirdSlot}</p>
-                            </span>
-                            <p className='text-foreground'>{loadout.thirdAttachment}</p>
-                        </div>
-                        <div className='w-full flex justify-between items-center text-primary'>
-                            <span className='flex gap-3 items-center'>
-                                <GoDotFill /> <p className='text-foreground'>{loadout.fourthSlot}</p>
-                            </span>
-                            <p className='text-foreground'>{loadout.fourthAttachment}</p>
-                        </div>
-                        <div className='w-full flex justify-between items-center text-primary'>
-                            <span className='flex gap-3 items-center'>
-                                <GoDotFill /> <p className='text-foreground'>{loadout.fifthSlot}</p>
-                            </span>
-                            <p className='text-foreground'>{loadout.fifthAttachment}</p>
-                        </div>
-                    </div >
-                    <p className='max-w-2xl mx-auto py-5 pl-5'>{loadout.description}</p>
+                <div className='flex flex-col  md:flex-row-reverse gap-5 md:gap-10 mt-5 md:mt-10 mb-10  justify-between'>
+                    <div className='md:w-[35%] w-full space-y-4 border rounded-md bg-[#101012] p-5'>
+                        <h3 className="text-xl font-bold">Attachments</h3>
+                        <Separator />
+                        <div className=' w-full flex flex-col gap-4'>
+                            <div className='w-full flex justify-between items-center text-primary '>
+                                <span className='flex gap-3 items-center'>
+                                    <GoDotFill /> <p className='text-foreground'>{loadout.firstSlot}</p>
+                                </span>
+                                <p className='text-foreground'>{loadout.firstAttachment}</p>
+                            </div>
+                            <div className='w-full flex justify-between items-center text-primary'>
+                                <span className='flex gap-3 items-center'>
+                                    <GoDotFill /> <p className='text-foreground'>{loadout.secondSlot}</p>
+                                </span>
+                                <p className='text-foreground'>{loadout.secondAttachment}</p>
+                            </div>
+                            <div className='w-full flex justify-between items-center text-primary'>
+                                <span className='flex gap-3 items-center'>
+                                    <GoDotFill /> <p className='text-foreground'>{loadout.thirdSlot}</p>
+                                </span>
+                                <p className='text-foreground'>{loadout.thirdAttachment}</p>
+                            </div>
+                            <div className='w-full flex justify-between items-center text-primary'>
+                                <span className='flex gap-3 items-center'>
+                                    <GoDotFill /> <p className='text-foreground'>{loadout.fourthSlot}</p>
+                                </span>
+                                <p className='text-foreground'>{loadout.fourthAttachment}</p>
+                            </div>
+                            <div className='w-full flex justify-between items-center text-primary'>
+                                <span className='flex gap-3 items-center'>
+                                    <GoDotFill /> <p className='text-foreground'>{loadout.fifthSlot}</p>
+                                </span>
+                                <p className='text-foreground'>{loadout.fifthAttachment}</p>
+                            </div>
+                        </div >
+                    </div>
+                    <div className='flex flex-col gap-4 justify-between md:w-[65%] w-full  border rounded-md bg-[#101012] p-5'>
+                        <h3 className="text-xl font-bold">Description</h3>
+                        <Separator />
+                        <p className='mb-5 text-lg'>{loadout.description}</p>
+                    </div>
                 </div>
             </div>
         </>
